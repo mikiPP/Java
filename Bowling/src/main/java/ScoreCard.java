c89b6a9d9d9ff9fe5c4644b0077fdab74ec96d61
Miguel Angel Palao Palmer <miki.palao.palmer@gmail.com>
2019-01-30 20:58:59 +0100

    Pasa todos los casos test

--- Bowling/src/main/java/ScoreCard.java
+++ Bowling/src/main/java/ScoreCard.java
@@ -11,6 +11,7 @@ public class ScoreCard {
     private int SPARE = 10;
     private int ZERO = 0;
 
+
     /*----------------Constructor---------------------*/
 
     ScoreCard(){}
@@ -42,49 +43,89 @@ public class ScoreCard {
 
         score = getSTRIKE();
 
-        for(int i = 1;i < 3;i++){
-            if (getSpecialRatings().indexOf(partida.charAt(position + i)) == -1){
-                score += getNumericalRatings().indexOf(partida.charAt(position+i));
-            }else{
-                if(partida.charAt(i)== 'X'){
-                    score  += this.getSTRIKE();
+        try{
+
+            for(int i = 1;i < 3;i++){
+                if (getSpecialRatings().indexOf(partida.charAt(position + i)) == -1){
+                    score += this.calculatePinsValue(partida,position+i);
                 }else{
-                    score += this.getSPARE() -  getNumericalRatings().indexOf(partida.charAt(position+i-1));
+                    if(partida.charAt(position + i)== 'X'){
+                        score  += this.getSTRIKE();
+                    }else{
+                        score += this.getSPARE() -  getNumericalRatings().indexOf(partida.charAt(position+i-1));
+                    }
                 }
             }
-        }
-        return score;}
+        }catch (StringIndexOutOfBoundsException e){
+            return this.ZERO;}
+
+        finally {
+            return score; }
+    }
 
 
     public int calculateSpareValue(String partida,int position){
-        int score = 0;
+        int score;
 
         score = getSPARE() - getNumericalRatings().indexOf(partida.charAt(position-1));
+        try{
+            if (getSpecialRatings().indexOf(partida.charAt(position + 1)) == -1){
+                score += this.calculatePinsValue(partida,position+1);;
+            }else{
+                score  += this.getSTRIKE();}
+        }catch (StringIndexOutOfBoundsException e){
+        ;}
+        finally {
+            return score; }
+        }
 
-        if (getSpecialRatings().indexOf(partida.charAt(position + 1)) == -1){
-            score += getNumericalRatings().indexOf(partida.charAt(position+1));
-        }else{
-            score  += this.getSTRIKE();}
-        return score;
+    public  int calculatePinsValue(String partida,int posicion){
+        return  getNumericalRatings().indexOf(partida.charAt(posicion));
     }
 
 
     public int calculateScore(String partida){
+        int finalScore = 0;
+        int ball=0;
 
-        int score = 0;
 
         for (int i = 0;i<partida.length();i++) {
+            if (ball < 18) {
 
+                if (getSpecialRatings().indexOf(partida.charAt(i)) == -1) {
+                        finalScore += calculatePinsValue(partida, i);
+                        ball++;
 
-            if (getSpecialRatings().indexOf(partida.charAt(i)) == -1) {
-                score += getNumericalRatings().indexOf(partida.charAt(i));
-            }else{
-                if(partida.charAt(i)== 'X'){
-                    score  += calculateStrikeValue(partida,i);
-                }else{
-                    score += calculateSpareValue(partida,i);
+                } else {
+                    if (partida.charAt(i) == 'X') {
+                            finalScore += calculateStrikeValue(partida, i);
+                            ball += 2;
+
+                    } else {
+                            finalScore += calculateSpareValue(partida, i);
+                            ball ++;
+                    }
                 }
+            }else{
+                finalScore += calculateLastThreeBalls(partida,i);
             }
-        }
+           }
+
+        return finalScore;}
+
+
+        public int calculateLastThreeBalls(String partida,int position){
+            int score;
+
+                if (getSpecialRatings().indexOf(partida.charAt(position)) == -1) {
+                    score = calculatePinsValue(partida, position);
+
+                } else if (partida.charAt(position) == '/'){
+                    score = getSPARE() - getNumericalRatings().indexOf(partida.charAt(position-1));
+                }else{
+                    score = getSTRIKE();
+                }
+
+
         return score;}
 }
--- Bowling/src/test/java/ScoreCardTest.java
+++ Bowling/src/test/java/ScoreCardTest.java
@@ -33,6 +33,11 @@ public class ScoreCardTest {
         String puntuacion ="9-9-9-9-9-9-9-9-9-9-";
         int total = 90;
         assertEquals(total,tarjeta.calculateScore(puntuacion));
+
+        puntuacion = "9-3561368153258-7181";
+        total = 82;
+
+        assertEquals(total, tarjeta.calculateScore(puntuacion));
     }
 
     @Test
@@ -46,7 +51,98 @@ public class ScoreCardTest {
     @Test
     public void getSpareValueTest() {
         String puntuacion = "1/345123451234512345";
+        String frame ="/";
         assertEquals(70,tarjeta.calculateScore(puntuacion));
+
     }
+
+    @Test
+    public void TotalScoreStrikeTest() {
+        /* If on his first try in the frame he knocks down all the
+         * pins, this is called a “strike”. His turn is over, and his
+         * score for the frame is ten plus the simple total of the
+         * pins knocked down in his next two rolls.
+         * If he gets a spare or strike in the last (tenth) frame,
+         * the bowler gets to throw one or two more bonus balls,
+         * respectively. - These bonus throws are taken as part of
+         * the same turn. If the bonus throws knock down all the
+         * pins, the process does not repeat: the bonus throws are
+         * only used to calculate the score of the final frame.
+         */
+
+        String pins = "X9-9-9-9-9-9-9-9-9-";
+        int total = 100;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+       // two extra final rolls
+        pins = "9-9-9-9-9-9-9-9-9-X9-";
+        total = 100;
+
+        assertEquals(total,tarjeta.calculateScore(pins));
+
+        pins = "X9-X9-9-9-9-9-9-9-";
+        total = 110;
+        assertEquals(total,tarjeta.calculateScore(pins));
+
+        // two strikes in a row is a double
+        pins = "XX9-9-9-9-9-9-9-9-";
+        total = 120;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        // three strikes in a row is a triple
+        pins = "XXX9-9-9-9-9-9-9-";
+        total = 141;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        pins = "XXXXXXXXXXXX";
+        total = 300;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        // two strikes in extra rolls
+        pins = "9-9-9-9-9-9-9-9-9-XXX";
+        total = 111;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+    }
+
+
+    @Test
+    public void TotalScoreSpareTest() {
+        /*
+         * If in two tries he knocks them all down, this is called
+         * a “spare” and his score for the frame is ten plus the
+         * number of pins knocked down on his next throw (in
+         * his next turn).
+         * If he gets a spare or strike in the last (tenth) frame,
+         * the bowler gets to throw one or two more bonus balls,
+         * respectively. - These bonus throws are taken as part of
+         * the same turn. If the bonus throws knock down all the
+         * pins, the process does not repeat: the bonus throws are
+         * only used to calculate the score of the final frame.
+         */
+
+        String pins = "5/5/5/5/5/5/5/5/5/5/5";
+        int total = 150;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        pins = "9-3/613/815/-/8-7/8/8";
+        total = 131;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        pins = "8/549-XX5/53639/9/X";
+        total = 149;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        pins = "8/549-XX3453639/9/X";
+        total = 136;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+        // spare in extra roll
+        pins = "X5/X5/XX5/--5/X5/";
+        total = 175;
+        assertEquals(total, tarjeta.calculateScore(pins));
+
+    }
+
 
 }
